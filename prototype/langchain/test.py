0d2f22af657ef94c009f86ab840f903bdc859980
Guilherme Luz <guilhermeluz@airmac.local>
2025-03-14 17:55:15 -0300

    inicio dos testes com langchain

--- prototype/langchain/test.py
+++ prototype/langchain/test.py
@@ -1,17 +1,26 @@
 #%%
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 import dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
+import os
 
-dotenv.load_dotenv()
 
-model = OpenAI("gpt-4o-mini")
+model = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 messages = [
     SystemMessage("Translate the following from English into Italian")
-    ,HumanMessage("hi!")
+    ,HumanMessage("hey bitch!")
 ]
 
+
+model.invoke(messages)
 response = model.invoke(messages)
 print(response.content)
 
